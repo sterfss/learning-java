2c8b87737ebd1b25b557459c4edc9c9c8f9c2fe8
sterfss <steffaniesilva2008@gmail.com>
2025-03-17 17:23:51 -0300

    Valores testes adicionados (funciona!!!)

--- AC2_BONIFICAÇÃO/AC2_BONIFICAÇÃO/src/App.java
+++ AC2_BONIFICAÇÃO/AC2_BONIFICAÇÃO/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Conta conta1 = new Conta ("624.482.444-24", 003, 15000);
+        
+        conta1.mostrarDados();
+
+        conta1.sacar(5000);
+
+        conta1.mostrarSaldo();
+
+        conta1.depositar(700);
+
+        conta1.mostrarSaldo();
     }
 }
