ccfa908dc1149e5deeb4102f5259aa29a5e48a5d
sterfss <steffaniesilva2008@gmail.com>
2025-03-22 14:52:03 -0300

    teste com a funcionalidade dos métodos

--- ATVCinema/Projeto Cinema/src/Elenco.java
+++ ATVCinema/Projeto Cinema/src/Elenco.java
@@ -2,4 +2,10 @@ public class Elenco {
     public Filme filme;
     public Ator ator;
     public boolean atorPrincipal;
+
+    public Elenco(Filme filme, Ator ator, boolean atorPrincipal){
+        this.filme = null;
+        this.ator = null;
+        this.atorPrincipal = false;
+    }
 }
--- ATVCinema/Projeto Cinema/src/Main.java
+++ ATVCinema/Projeto Cinema/src/Main.java
@@ -1,4 +1,4 @@
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
 
         Genero g1 = new Genero(1, "Ação", true);
@@ -7,10 +7,15 @@ public class App {
         Filme f1 = new Filme(g1, 1, 1.40, "Busca Implacável");
         Filme f2 = new Filme(g2, 2, 1.60, "Todo mundo em Pânico");
         
-        System.out.println("Informações do filme:");
+        Ator a1 = new Ator("Liam Neeson", 72, 1);
+        Ator a2 = new Ator("Ana Faris", 48, 2);
+
+        Elenco e1 = new Elenco(f1, a1, true);
+        Elenco e2 = new Elenco(f2, a1, false);
+
+        System.out.println("Informações do Filme:");
         f1.dadosFilme();
         System.out.println("");
-        System.out.println("Informações do filme:");
         f2.dadosFilme();
 
     }
