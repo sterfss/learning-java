e3dbda80de038498f103724ad13cc2786eb9cdcf
Steffanie <steffaniesilva2008@gmail.com>
2025-05-10 21:11:32 -0300

    adicionando teste de operações com um objeto conta instanciado

--- LCPOO-AV2-pt1/av2-pt1/src/Account.java
+++ LCPOO-AV2-pt1/av2-pt1/src/Account.java
@@ -31,4 +31,22 @@ public class Account{
             balance += value;
         }
     }
+    public void doOperation(Operation operation){
+        //Precisamos verificar o tipo da operação
+        if(operation.getType() == 'D'){
+            balance += operation.getValue();
+            //Se não for do tipo D (Deposit), precisa ser do tipo W(Withdrawal)
+        }else if(operation.getType() == 'W'){
+            //Aqui é tipo: ok, é uma retirada, mas você tem saldo suficiente?
+            if(balance >= operation.getValue()){
+                balance -= operation.getValue();
+                //Caso a verificação do saldo dê insuficiente, printará essa mensagem
+            }else{
+                System.out.println("Saldo insuficiente para operação");
+            }
+            //Caso a verificação do tipo de operação não seja nem D nem R, será uma operação inválida
+        }else{
+            System.out.println("Tipo de operação inválida");
+        }
+    }
 }
--- LCPOO-AV2-pt1/av2-pt1/src/Main.java
+++ LCPOO-AV2-pt1/av2-pt1/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Client c1 = new Client("Jhonas", "111.222.333-44", "Rua dos bobos, nº 0");
-        Account ac1 = new Account(c1, 1500.6);
+        Account ac1 = new Account(c1, 1000.6);
+        Operation op1 = new Operation('D', 500.0);
+        Operation op2 = new Operation('W', 500.0);
+        Operation op3 = new Operation('W', 2000.1);
 
-        ac1.showAccountData();
+        ac1.doOperation(op1);
+        ac1.doOperation(op2);
+        ac1.doOperation(op3);
+
+        System.out.println("Saldo final: " + ac1.getBalance() + "R$");
     }
 }
