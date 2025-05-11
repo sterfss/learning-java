3973c8c56e203337c6583d98a2535f5143f614af
Steffanie <steffaniesilva2008@gmail.com>
2025-05-11 17:35:02 -0300

    adicionando metodos da classe turma para encapsulamento

--- LCPOO-AV2-pt2/relacionamento-entre-classespt2/src/Class.java
+++ LCPOO-AV2-pt2/relacionamento-entre-classespt2/src/Class.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Class {
-    public Student student;
+    public ArrayList<Student> students;
     public String course;
 
-    
+    public Class(String course){
+        this.course = course;
+        this.students = new ArrayList<>();
+    }
+
+    public void setCourse(String course){
+        this.course = course;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public void addStudents(Student student){
+        students.add(student);
+    }
+
+    public List<Student> getStudents(){
+        return students;
+    }
 }
 
